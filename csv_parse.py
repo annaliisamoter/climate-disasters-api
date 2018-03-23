24f435564c4ae1ffcf21d155b13246adfc00b327
Anna Liisa Moter <servicemamma@gmail.com>
2018-03-23 13:46:22 -0700

    makes small edits after trying some things that didn't work.

--- csv_parse.py
+++ csv_parse.py
@@ -1,19 +1,85 @@
 import csv
+import json
+
+states = {
+    'AK': 'Alaska',
+    'AL': 'Alabama',
+    'AR': 'Arkansas',
+    'AS': 'American Samoa',
+    'AZ': 'Arizona',
+    'CA': 'California',
+    'CO': 'Colorado',
+    'CT': 'Connecticut',
+    'DC': 'District of Columbia',
+    'DE': 'Delaware',
+    'FL': 'Florida',
+    'FM': 'Marshall Islands',
+    'GA': 'Georgia',
+    'GU': 'Guam',
+    'HI': 'Hawaii',
+    'IA': 'Iowa',
+    'ID': 'Idaho',
+    'IL': 'Illinois',
+    'IN': 'Indiana',
+    'KS': 'Kansas',
+    'KY': 'Kentucky',
+    'LA': 'Louisiana',
+    'MA': 'Massachusetts',
+    'MD': 'Maryland',
+    'ME': 'Maine',
+    'MH': 'Marshall Islands',
+    'MI': 'Michigan',
+    'MN': 'Minnesota',
+    'MO': 'Missouri',
+    'MP': 'Northern Mariana Islands',
+    'MS': 'Mississippi',
+    'MT': 'Montana',
+    'NA': 'National',
+    'NC': 'North Carolina',
+    'ND': 'North Dakota',
+    'NE': 'Nebraska',
+    'NH': 'New Hampshire',
+    'NJ': 'New Jersey',
+    'NM': 'New Mexico',
+    'NV': 'Nevada',
+    'NY': 'New York',
+    'OH': 'Ohio',
+    'OK': 'Oklahoma',
+    'OR': 'Oregon',
+    'PA': 'Pennsylvania',
+    'PR': 'Puerto Rico',
+    'PW': 'Palau',
+    'RI': 'Rhode Island',
+    'SC': 'South Carolina',
+    'SD': 'South Dakota',
+    'TN': 'Tennessee',
+    'TX': 'Texas',
+    'UT': 'Utah',
+    'VA': 'Virginia',
+    'VI': 'Virgin Islands',
+    'VT': 'Vermont',
+    'WA': 'Washington',
+    'WI': 'Wisconsin',
+    'WV': 'West Virginia',
+    'WY': 'Wyoming'
+}
 
 
 def parses_csv(date_range=None, type=None):
     """parses fema data and returns dictionary of states with number of incidents"""
-
+    #first pass through, assume no parameters given.
     states_dictionary = {}
+    
     with open('DisasterDeclarationsSummaries.csv', 'r') as csv_file:
         csv_reader = csv.DictReader(csv_file)
         for line in csv_reader:
-            state = line['state']
-            states_dictionary.setdefault(state,0)
+            state_abbr = line['state']
+            state = states[state_abbr]
+            states_dictionary.setdefault(state, 0)
             states_dictionary[state] += 1
     
 
-    print(states_dictionary)
+    return states_dictionary
 
 
-
+parses_csv()
