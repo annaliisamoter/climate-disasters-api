310d01e5cc36ccf08286f396a3ffb490d364f02d
Anna Liisa Moter <servicemamma@gmail.com>
2018-03-24 11:46:38 -0700

    cleans up code, leaves functionality unchanged.

--- csv_parse.py
+++ csv_parse.py
@@ -1,5 +1,4 @@
 import csv
-import json
 
 #provides mapping reference of state abbreviations to full state names
 states = {
@@ -64,72 +63,11 @@ states = {
     'WY': 'Wyoming'
 }
 
-#initializes a dictionary by full state name
-# states_dictionary = {
-#     'Alaska': 0,
-#     'Alabama': 0,
-#     'Arkansas': 0,
-#     'American Samoa': 0,
-#     'Arizona': 0,
-#     'California': 0,
-#     'Colorado': 0,
-#     'Connecticut': 0,
-#     'District of Columbia': 0,
-#     'Delaware': 0,
-#     'Florida': 0,
-#     'Marshall Islands': 0,
-#     'Georgia': 0,
-#     'Guam': 0,
-#     'Hawaii': 0,
-#     'Iowa': 0,
-#     'Idaho': 0,
-#     'Illinois': 0,
-#     'Indiana': 0,
-#     'Kansas': 0,
-#     'Kentucky': 0,
-#     'Louisiana': 0,
-#     'Massachusetts': 0,
-#     'Maryland': 0,
-#     'Maine': 0,
-#     'Marshall Islands': 0,
-#     'Michigan': 0,
-#     'Minnesota': 0,
-#     'Missouri': 0,
-#     'Northern Mariana Islands': 0,
-#     'Mississippi': 0,
-#     'Montana': 0,
-#     'North Carolina': 0,
-#     'North Dakota': 0,
-#     'Nebraska': 0,
-#     'New Hampshire': 0,
-#     'New Jersey': 0,
-#     'New Mexico': 0,
-#     'Nevada': 0,
-#     'New York': 0,
-#     'Ohio': 0,
-#     'Oklahoma': 0,
-#     'Oregon': 0,
-#     'Pennsylvania': 0,
-#     'Puerto Rico': 0,
-#     'Palau': 0,
-#     'Rhode Island': 0,
-#     'South Carolina': 0,
-#     'South Dakota': 0,
-#     'Tennessee': 0,
-#     'Texas': 0,
-#     'Utah': 0,
-#     'Virginia': 0,
-#     'Virgin Islands': 0,
-#     'Vermont': 0,
-#     'Washington': 0,
-#     'Wisconsin': 0,
-#     'West Virginia': 0,
-#     'Wyoming': 0
-#     }
-
 
 def parses_csv(begin=None, end=None, incident_filter=None):
-    """parses fema data and returns dictionary of states with number of incidents"""
+    """parses fema data from csv file and returns dictionary of states with
+    number of incidents."""
+
     states_dictionary = {}
     begin = int(begin)
     end = int(end)
