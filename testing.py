63e851e765c3ac3ecba7b63dd73ce7b3d20d8152
Anna Liisa Moter <servicemamma@gmail.com>
2018-03-26 10:17:54 -0700

    adds a few basic tests.

--- testing.py
+++ testing.py
@@ -1,9 +1,10 @@
 import os
 import unittest
+from csv_parse import parses_csv
 from server import app
 from flask import json, jsonify
 
-class FlaskTests(unittest.TestCase):
+class FlaskTestcase(unittest.TestCase):
     """Testing of app routes, both functional and unittest."""
 
     def setUp(self):
@@ -15,22 +16,25 @@ class FlaskTests(unittest.TestCase):
 
     def tearDown(self):
         """Do at end of every test."""
-
+        #no db to tear down in this case
 
     def test_index(self):
         """tests rendering index"""
 
         result = self.client.get("/")
         self.assertEqual(result.status_code, 200)
-        #self.assertIn('index.html', result.data)
+        #self.assertIn('Climate Disasters', result.data)
 
-    #def test_api_call(self):
-        """tests api call route"""
-        #result = self.client.get("/api-call")
-        #self.
+    def test_api_call(self):
+        """tests api call route returning proper jason"""
+        result = self.client.get("/api-call?begin=2014&end=2014&type-of-disaster=Chemical")
+        self.assertEqual(result.status_code, 200)
 
+    def test_csv_parse_funct(self):
+        """tests the parses_csv function"""
+        result = parses_csv(2014, 2014, 'Chemical')
+        self.assertEqual(result, {'West Virginia': 9})
 
-        
 
 
 if __name__ == '__main__':
