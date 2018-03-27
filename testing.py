b5cd30f52476b492372af229668254c44a56aada
Anna Liisa Moter <servicemamma@gmail.com>
2018-03-27 09:28:28 -0700

    adds a few additional tests, fixes tests, all tests pass.

--- testing.py
+++ testing.py
@@ -1,8 +1,8 @@
-import os
 import unittest
 from csv_parse import parses_csv
 from server import app
-from flask import json, jsonify
+import json
+
 
 class FlaskTestcase(unittest.TestCase):
     """Testing of app routes, both functional and unittest."""
@@ -17,23 +17,34 @@ class FlaskTestcase(unittest.TestCase):
     def tearDown(self):
         """Do at end of every test."""
         #no db to tear down in this case
+        pass
 
     def test_index(self):
         """tests rendering index"""
-
-        result = self.client.get("/")
+        result = self.client.get("/", follow_redirects=True)
         self.assertEqual(result.status_code, 200)
-        #self.assertIn('Climate Disasters', result.data)
+        data_string = str(result.data)
+        self.assertIn('Climate Disasters', data_string)
 
     def test_api_call(self):
-        """tests api call route returning proper jason"""
+        """tests api call route returning proper json"""
         result = self.client.get("/api-call?begin=2014&end=2014&type-of-disaster=Chemical")
         self.assertEqual(result.status_code, 200)
+        print(result)
+        self.assertEqual({'West Virginia': 9}, json.loads(result.data))
 
     def test_csv_parse_funct(self):
         """tests the parses_csv function"""
         result = parses_csv(2014, 2014, 'Chemical')
         self.assertEqual(result, {'West Virginia': 9})
+
+    def map_rendering(self):
+        """tests that map rerenders upon api call"""
+        pass
+
+    def test_form_inputs(self):
+        """tests that form inputs are captured and sent to server as part of api call"""
+        pass
 
 
 
